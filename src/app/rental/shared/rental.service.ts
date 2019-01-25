86daab90fa0c3d2ea7bc9b9d68d71322105ef52a
Andrea Charles <FZJTB1@NAM.corp.gm.com>
2019-01-25 14:50:47 -0600

    add pipes

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rental } from './rental.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
+export class RentalService{
+
+	constructor(private http: HttpClient){}
+
+	public getRentalById(rentalId: string): Observable<any>{
+		return this.http.get('/api/v1/rentals/' + rentalId);
+	}
+
+	public getRentals(): Observable<any> {
+		return this.http.get('/api/v1/rentals');
+	}
+}
+
+
+/*
 export class RentalService{
 	
 	private rentals: Rental[] = [
@@ -68,7 +84,7 @@ export class RentalService{
 				});
 				observer.next(foundRental);
 			}, 500);
-		})
+		});
 	}
 
 	public getRentals(): Observable<Rental[]> {
@@ -79,3 +95,4 @@ export class RentalService{
 		});
 	}
 }
+*/
